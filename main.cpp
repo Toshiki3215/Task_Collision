1f87a2fef4a6b6ccdf869e1572b0c137f881bcad
HatouToshiki <k021g1448@g.neec.ac.jp>
2022-11-25 15:18:38 +0900

    線と円の当たり判定

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "DxLib.h"
+#include "Vector2.h"
 
 // ウィンドウのタイトルに表示する文字列
 const char TITLE[] = "LE2B_19_ハトウトシキ: タイトル";
@@ -10,7 +11,7 @@ const int WIN_WIDTH = 600;
 const int WIN_HEIGHT = 400;
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine,
-                   _In_ int nCmdShow) {
+	_In_ int nCmdShow) {
 	// ウィンドウモードに設定
 	ChangeWindowMode(TRUE);
 
@@ -40,13 +41,33 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 
 
 	// ゲームループで使う変数の宣言
+	Vector2 lineA(100, 300);
+	Vector2 lineB(500, 300);
 
+	Vector2 circle(300, 100);
+	int r = 50;
+
+	int cr = GetColor(255, 255, 255);
+
+	Vector2 lineV = lineB - lineA;
+	Vector2 lineCircleVA = circle - lineA;
+	Vector2 lineCircleVB = circle - lineB;
+
+	Vector2 lineVnorm = lineV;
+
+	bool trigger1 = false;
+	bool trigger2 = false;
+
+	float lineLengthA = lineCircleVA.length();
+	float lineLengthB = lineCircleVB.length();
+
+	int n = lineVnorm.x * lineCircleVA.y - lineCircleVA.x * lineVnorm.y;
 
 	// 最新のキーボード情報用
-	char keys[256] = {0};
+	char keys[256] = { 0 };
 
 	// 1ループ(フレーム)前のキーボード情報
-	char oldkeys[256] = {0};
+	char oldkeys[256] = { 0 };
 
 	// ゲームループ
 	while (true) {
@@ -59,9 +80,106 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 		//---------  ここからプログラムを記述  ----------//
 
 		// 更新処理
+		if (keys[KEY_INPUT_W] == 1 || keys[KEY_INPUT_S] == 1)
+		{
+			if (keys[KEY_INPUT_W] == 1)
+			{
+				circle.y -= 10;
+			}
+			else if (keys[KEY_INPUT_S] == 1)
+			{
+				circle.y += 10;
+			}
+		}
 
+		if (keys[KEY_INPUT_A] == 1 || keys[KEY_INPUT_D] == 1)
+		{
+			if (keys[KEY_INPUT_A] == 1)
+			{
+				circle.x -= 10;
+			}
+			else if (keys[KEY_INPUT_D] == 1)
+			{
+				circle.x += 10;
+			}
+		}
+
+		lineCircleVA = circle - lineA;
+		lineCircleVB = circle - lineB;
+
+		lineLengthA = lineCircleVA.length();
+		lineLengthB = lineCircleVB.length();
+
+		lineVnorm.normalize();
+
+		n = lineVnorm.x * lineCircleVA.y - lineCircleVA.x * lineVnorm.y;
+
+		if (circle.y <= lineA.y)
+		{
+			n *= -1;
+		}
+
+		if (n <= r)
+		{
+			float dot01 = lineCircleVA.x * lineV.x + lineCircleVA.y * lineV.y;
+
+			float dot02 = lineCircleVB.x * lineV.x + lineCircleVB.y * lineV.y;
+
+			if (dot01 * dot02 <= 0.0f)
+			{
+				trigger1 = TRUE;
+			}
+			else
+			{
+				trigger1 = false;
+			}
+		}
+		else
+		{
+			trigger1 = false;
+		}
+
+		if (lineLengthA < r || lineLengthB < r)
+		{
+			trigger2 = TRUE;
+		}
+		else
+		{
+			trigger2 = false;
+		}
+
+		if (trigger1 == TRUE|| trigger2 == TRUE)
+		{
+			cr = GetColor(255, 0, 0);
+		}
+		else
+		{
+			cr = GetColor(255, 255, 255);
+		}
+
+		//ベクトルなし(斜め時非対応)
+		/*if (circle.x + r >= lineA.x && circle.x - r <= lineB.x)
+		{
+			if (lineA.y <= circle.y + r && lineA.y >= circle.y - r)
+			{
+				cr = GetColor(255, 0, 0);
+			}
+			else
+			{
+				cr = GetColor(255, 255, 255);
+			}
+		}
+		else
+		{
+			cr = GetColor(255, 255, 255);
+		}*/
 
 		// 描画処理
+		DrawLine(lineA.x, lineA.y, lineB.x, lineB.y, cr, TRUE);
+
+		DrawCircle(circle.x, circle.y, r, cr, TRUE);
+
+		DrawFormatString(0, 10, cr, "%d", n, TRUE);
 
 		//---------  ここまでにプログラムを記述  ---------//
 		// (ダブルバッファ)裏面
